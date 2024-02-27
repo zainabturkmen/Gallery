280338987ef48cdfc50a5c9f355d1786ae77613e
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-27 17:21:31 +0430

    adding some changes

--- app.js
+++ app.js
@@ -11,8 +11,9 @@ function getElement(selection) {
 function Gallery(element){
   this.list = [...element.querySelectorAll(".img")];
   this.modal = getElement(".modal");
-  this.modalImg = getElement(".main-img")
-  this.modalImges = getElement(".modal-images")
+  this.modalImg = getElement(".main-img");
+  this.modalImges = getElement(".modal-images");
+  
 }
 
 const nature = new Gallery(getElement(".nature"));
