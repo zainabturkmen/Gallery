e37ea72035d87b40d75041de4d6bc79f2e46ae8b
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-27 17:16:06 +0430

    adding some changes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function getElement(selection) {
 
 function Gallery(element){
   this.list = [...element.querySelectorAll(".img")];
+  console.log(this.list);
 }
 
 const nature = new Gallery(getElement(".nature"));
