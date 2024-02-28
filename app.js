06b7157c0bcb263e933eff96015d08c83c4ea2c3
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-28 06:54:12 +0430

    adding some changes

--- app.js
+++ app.js
@@ -37,10 +37,11 @@ function Gallery(element){
 Gallery.prototype.openModal = function(selectedImage, list){
   this.setMainImages(selectedImage);
   this.modalImges.innerHTML = list.map(function(image){
-    console.log(image);
+    return `<img src="${image.src}" title="${image.title}" 
+    dataset-id="${image.dataset.id}" class="${selectedImage.dataset.id === image.dataset.id? "modal-img selected": "modal-img"}"/>`
   }).join("");
   this.modal.classList.add("open")
-}
+};
 
 Gallery.prototype.setMainImages = function(selectedImage){
   this.modalImg.src = selectedImage.src 
