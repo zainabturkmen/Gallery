ead5ae428f028058e6a9ba59cb57a68e73542d13
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-02-28 08:33:37 +0430

    adding some changes

--- refactor.js
+++ refactor.js
@@ -2,7 +2,7 @@ function getElement(selection) {
     const element = document.querySelector(selection);
     if (element) {
       return element;
-    }
+    };
     throw new Error(
       `Please check "${selection}" selector, no such element exists`
     );
